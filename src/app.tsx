92f81fba2e3e3c14b5ca86858886e6a44dc95aa5
mentorvikash <mentorvikash@gmail.com>
2023-07-15 14:17:30 +0530

    config webpack for react application

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,12 @@
-export const App =  () => {
+import IMAGE from "./react.png";
+import SVGIMAGE from "./react.svg"
 
-    return <h1>This is our react app</h1>
-
-}
+export const App = () => {
+  return (
+    <>
+      <h1>This is our react app</h1>
+      <img src={IMAGE} alt="raact logo" height={300} width={300} />
+      <img src={SVGIMAGE} alt="raact logo" height={300} width={300} />
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./app";
+import './styles.css'
 
 ReactDOM.render(<App />, document.getElementById("root"))
+ 
