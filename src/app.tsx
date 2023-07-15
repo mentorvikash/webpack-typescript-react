3896e8fcd3efd19c8909f460c4cac9ae5dbd09e9
mentorvikash <mentorvikash@gmail.com>
2023-07-15 16:42:23 +0530

    add new change

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,8 @@ import Counter from './components/counter'
 
 export const App = () => {
   const name = "vikash singh"
-  
+  const name = "vikash singh"
+
   return (
     <>
       <h1>This is our react app {process.env.name}</h1>
