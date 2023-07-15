26aa08eb8827cc8a655c4bd18185d134ea2ea637
mentorvikash <mentorvikash@gmail.com>
2023-07-15 16:38:43 +0530

    added huskty and pre formated beform commit

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import SVGIMAGE from './react.svg'
 import Counter from './components/counter'
 
 export const App = () => {
+  const name = "vikash singh"
+  
   return (
     <>
       <h1>This is our react app {process.env.name}</h1>
@@ -24,3 +26,6 @@ export const App = () => {
 // eslint-confing-prettier disable eslint rule that conflicts with code formatting and eslint-plugin-prettier all prettier to define eslint rule
 // npm i -D prettier eslint-confing-prettier eslint-plugin-prettier
 // create .prettierrc.js
+
+// ensure no eslint error will pushed github
+// npm i -D husky lint-staged
